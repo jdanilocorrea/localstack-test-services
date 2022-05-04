1b4e35a35b91b0421cd9c1af9783da0f26d875f6
jcorreatavares <jd.correa.tavares@gmail.com>
2022-05-04 02:19:28 -0300

    test create bucket

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
+import logging
 import boto3
 from botocore.exceptions import ClientError
+import json
 import os
 
 AWS_REGION = 'us-east-1'
 AWS_PROFILE = 'localstack'
 ENDPOINT_URL = os.environ.get('LOCALSTACK_ENDPOINT_URL')
 
+# logger config
+logger = logging.getLogger()
+logging.basicConfig(level=logging.INFO,
+                    format='%(asctime)s: %(levelname)s: %(message)s')
+
 boto3.setup_default_session(profile_name=AWS_PROFILE)
 
-loaclstack_client = boto3.client("s3", region_name=AWS_REGION,
+s3_client = boto3.client("s3", region_name=AWS_REGION,
                          endpoint_url=ENDPOINT_URL)
-#-------------------------------------------------------------------------
-# endpoint_url = "http://localhost.localstack.cloud:4566"
-# # alternatively, to use HTTPS endpoint on port 443:
-# # endpoint_url = "https://localhost.localstack.cloud"
 
-# def main():
-#     client = boto3.client("lambda", endpoint_url=endpoint_url)
-#     result = client.list_functions()
-#     print(result)
 
-# if __name__ == "__main__":
-#     main()
-#--------------------------------------------------------------------------
+def create_bucket(bucket_name):
+    """
+    Creates a S3 bucket.
+    """
+    try:
+        response = s3_client.create_bucket(
+            Bucket=bucket_name)
+    except ClientError:
+        logger.exception('Could not create S3 bucket locally.')
+        raise
+    else:
+        return response
+
+
+def main():
+    """
+    Main invocation function.
+    """
+    bucket_name = "hands-on-cloud-localstack-bucket"
+    logger.info('Creating S3 bucket locally using LocalStack...')
+    s3 = create_bucket(bucket_name)
+    logger.info('S3 bucket created.')
+    logger.info(json.dumps(s3, indent=4) + '\n')
+
+
+if __name__ == '__main__':
+    main()
